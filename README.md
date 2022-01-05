20fc850a2fd95449d14a6d5861f175151e4949c5
fecton <bopongamer@gmail.com>
2022-01-05 23:17:05 +0200

    A lots of updates 1. Updated .gitignore 2. Added README.md with screenshots 3. Created two folders: images (icons for program and readme_images) 4. Added status features 5. The submit button moved to center

---
+++ README.md
@@ -0,0 +1,50 @@
+# YouTube music downloader on Python ♥
+
+## Installing necessary modules:
+```
+pip install youtube_dl pafy
+```
+## Running:
+```
+python main.py
+```
+
+#
+## Main window:
+<img src="readme_images/main_window.png" width=400>
+
+## Downloading:
+<img src="readme_images/downloading.png" width=400>
+<img src="readme_images/music_downloaded.png" width=400>
+
+# Errors
+## Bad URL:
+<img src="readme_images/bad_url.png" width=400>
+
+> It raised when you entered not youtube site or simple text
+
+## Module error:
+<img src="readme_images/module_error.png" width=400>
+
+> It raises because pafy module have a disadvantage with dislike count, let's fix it!
+
+> Get that path in the running console
+<img src="readme_images/module_fix_1.png" width=400>
+
+> Go to that file
+<img src="readme_images/module_fix_2.png" width=400>
+
+> Let's see the problem, there's here in lines 53-54:
+<img src="readme_images/module_fix_3.png" width=400>
+
+> Let's correct it:
+<img src="readme_images/module_fix_4.png" width=400>
+```
+self._likes = self._ydl_info.get('like_count', 0)
+self._dislikes = self._ydl_info.get('dislike_count', 0)
+```
+
+
+## Save and try again :)
+
+### Thanks for using!
--- main.py
+++ main.py
@@ -4,41 +4,49 @@ from tkinter import messagebox
 
 def empty_url_entry():
 	URL_entry.delete(0, "end")
-	return
 
 def download_audio():
 	url = URL_entry.get()
 	try:
+		window.title("Downloader: started downloading")
 		audio = pafy.new(url=url)
 	except ValueError:
 		messagebox.showerror(title="Error", message="Bad URL!")
+		window.title("Downloader: downloading cancelled")
 		return
+	except KeyError:
+		messagebox.showerror(title="Error", message="KeyError! Read README.md!")
+		window.title("Downloader: CRITICAL ERROR! READ README.md!!!!!")
 
-	audio_title = url.title
-
+	audio_title = audio.title
+	print("\nSong name: %s" % audio_title)
+	
 	get_audio = audio.getbestaudio()
+
 	get_audio.download()
-	print("Song name: %s" % audio_title)
+
+	messagebox.showinfo(title="Completed", message="✅Your song was downloaded! Enjoy! <3")
+	window.title("Downloader: ready for downloading")
 
 
 window = tkinter.Tk()
-window.title("Youtube music downloader")
+window.title("Downloader: ready for downloading")
 window.geometry("500x150")
 window.resizable(False, False)
 window.config(bg="#6cc6d3")
 
-logo = tkinter.PhotoImage(file='icon.png')
+logo = tkinter.PhotoImage(file='images/icon.png')
 window.call('wm', 'iconphoto', window._w, logo)
 
 URL_entry = tkinter.Entry(bd=0, bg="#F6F7F9", highlightthickness=0, font=("Arial-BoldMT",int(12.0)))
 URL_entry.place(x=50, y=20, width=400, height=35)
 
-trash_icon = tkinter.PhotoImage(file="trash.png", height=30, width=30)
+trash_icon = tkinter.PhotoImage(file="images/trash.png", height=30, width=30)
 
 trash_button = tkinter.Button(image=trash_icon, borderwidth=0, highlightthickness=0, command=empty_url_entry, relief="flat")
-trash_button.place(x=460, y=20, width=30, height=30)
+trash_button.place(x=460, y=20, width=30, height=35)
 
 button1 = tkinter.Button(text="Download", command=download_audio)
-button1.place(x=20, y=70, width=80, height=50)
+button1.place(x=200, y=90, width=80, height=50)
 
 window.mainloop()
